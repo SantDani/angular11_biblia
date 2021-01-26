78503f7ab347b93b2302b5fead4f076ec114994e
SantiUAB <profSantiRP@gmail.com>
2021-01-26 16:15:02 +0100

    Interpolación, variables

---
+++ MiPrimeraApp/src/app/saludo/saludo.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+//Decorador propio de angular
+// indica que ya no es una clase typescript, se ha convertido en un compnente de angular
+@Component({
+  /**selector: propiedad que nos permite utilizar este componente
+   * en cualquier punto de la applicacion con el tag <saludo></saludo>. */
+  selector: 'saludo',
+
+  /** templateUrl indicamos que fichero HTML va tener este componente,*/
+  templateUrl: './saludo.component.html',
+  /**Template: nos permite poner directamente el codigo HTML */
+  //template: '<h1> k mas ve </h1>',
+
+  /** `` comillas abiertas nos permite realizar un template literal,
+   * que nos permite salto de linea */
+  // template: `<h1> k mas ve </h1>
+  // <h2>subtitulo<h2>`,
+
+  /**styleUrls: alberga una array de hojas de estilo,
+   * esto permite encapsular estilos
+   */
+  styleUrls: ['./saludo.component.css']
+  //style: [`codigo CSS. Funciona igual que la propiedad template.`]
+})
+
+/**Clase de Type script. */
+export class SaludoComponent implements OnInit {
+
+  /**Las variables tienen que estar inicializadas  */
+  mensaje: string ;
+
+  constructor() {
+    this.mensaje = 'Se los va a garchar a todos';
+
+    setTimeout (() => {
+      this.mensaje = 'Garchados en 3 s, easy'
+
+    }, 3000)
+   }
+
+  ngOnInit(): void {
+  }
+
+}
