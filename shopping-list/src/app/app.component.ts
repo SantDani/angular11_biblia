53f53e793303ee34cc6712ca169f333d550667c9
SantiUAB <profSantiRP@gmail.com>
2021-02-02 00:23:02 +0100

    solucinado by me

--- shopping-list/src/app/app.component.ts
+++ shopping-list/src/app/app.component.ts
@@ -10,10 +10,18 @@ export class AppComponent {
   title = 'shopping-list';
 
   products : Product[];
+  productsBuyed : Product[];
+
 
 
   constructor(){
     this.products = [];
+    this.products.push(new Product('Fajita', 50, 'Mexico'));
+    this.products.push(new Product('Pan del dia', 1, '365'));
+    this.products.push(new Product('Fanta', 50, 'Bebida'));
+    this.products.push(new Product('Queso', 50, 'Lactios'));
+    this.products.push(new Product('Kebab', 50, 'Amigo'));
+    this.productsBuyed = [];
   }
   lisenerNewProduc($event: Product){
     console.log("Nuevo Producto add  -- " + $event.name);
@@ -21,8 +29,14 @@ export class AppComponent {
   }
 
   lisenerBuyProduct($event: Product){
-    console.log ("Producto compado" + $event.name);
+    console.log ("Producto compado " + $event.name);
+    //$event.buyed = true;
     $event.buyed = true;
+    this.productsBuyed.push($event);
 
+  }
+
+  lisenerDeleteProduct($event : Product){
+    this.products.push($event);
   }
 }
--- shopping-list/src/app/list-products/list-products.component.ts
+++ shopping-list/src/app/list-products/list-products.component.ts
@@ -12,20 +12,47 @@ export class ListProductsComponent implements OnInit {
   @Input()
   inputProducts: Product[];
   @Input()
-  inputMostrarComprados: boolean;
+  inputProductsBuy : Product[];
 
   @Output()
     outEventBuyProduct: EventEmitter<Product>;
+
+  @Output()
+    outEvenetDeleteProduct : EventEmitter<Product>;
   constructor() {
     this.inputProducts = [];
     this.outEventBuyProduct = new EventEmitter();
-    this.inputMostrarComprados = true;
+    this.inputProductsBuy = [];
+    this.outEvenetDeleteProduct = new EventEmitter();
   }
 
   ngOnInit(): void {
   }
 
   buyProduct(product : Product){
+    // this.outEventBuyProduct.emit(product);
+    product.buyed = true;
+    this.del(this.inputProducts, product);
     this.outEventBuyProduct.emit(product);
+  }
+
+  deleteProduct(product : Product){
+    product.buyed = false;
+    this.del(this.inputProductsBuy, product);
+    this.outEvenetDeleteProduct.emit(product);
+
+  }
+
+  del(arrayProduct: Product[], product : Product){
+
+    const index = arrayProduct.indexOf(product);
+    console.log("index product  " + index);
+    console.log(" name product " + product.name);
+    if(index > -1){
+      console.log("elimino proucto " + product.name);
+      //product.buyed = false;
+      arrayProduct.splice(index, 1);
+
+    }
   }
 }
