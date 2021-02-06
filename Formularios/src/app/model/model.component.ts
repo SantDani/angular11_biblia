858b828710cacd5e10a67cdef2e3ac43c10e4229
SantiUAB <profSantiRP@gmail.com>
2021-02-06 11:48:46 +0100

    S8.4 Validaciones simples enfocadas al CSS (para el usuario sepa que el campo es incorrecto o correcto)

--- Formularios/src/app/model/model.component.ts
+++ Formularios/src/app/model/model.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-model',
@@ -14,13 +14,15 @@ export class ModelComponent implements OnInit {
   constructor() {
     this.formulario = new FormGroup(
       {
-        nombre: new FormControl(),
-        apellido: new FormControl(),
-        edad: new FormControl(),
-        dni: new FormControl(),
-        password: new FormControl(),
-        repite_password: new FormControl(),
-        email: new FormControl(),
+        nombre: new FormControl('', [
+          Validators.required,
+        ]),
+        apellido: new FormControl(''),
+        edad: new FormControl(''),
+        dni: new FormControl(''),
+        password: new FormControl(''),
+        repite_password: new FormControl(''),
+        email: new FormControl(''),
       }
     );
    }
