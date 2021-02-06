c960c47257340dbeb7dba6a2992394b3ae4a88f3
SantiUAB <profSantiRP@gmail.com>
2021-02-06 10:57:00 +0100

    S8.1 bootstrap instalado

--- Formularios/src/app/app-routing.module.ts
+++ Formularios/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { ModelComponent } from './model/model.component';
+import { TemplateComponent } from './template/template.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch:'full' , redirectTo: '/model'},
+  {path: 'template', component: TemplateComponent},
+  {path: 'model', component: ModelComponent},
+  {path: '**', redirectTo: 'model'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
