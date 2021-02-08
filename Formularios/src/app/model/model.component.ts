8d2ba706833966774c9e15abb22e64722b01103c
SantiUAB <profSantiRP@gmail.com>
2021-02-08 15:28:22 +0100

    S8.6 Formulario model (Reactivo) Mostrar errores HTML parte 2 Value change ( subscribir a cambios) (observer)

--- Formularios/src/app/model/model.component.ts
+++ Formularios/src/app/model/model.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+//recupera cada tecla que pulsamos
+import { debounceTime } from 'rxjs/operators';
+
 @Component({
   selector: 'app-model',
   templateUrl: './model.component.html',
@@ -24,14 +27,19 @@ export class ModelComponent implements OnInit {
       {
         nombre: new FormControl('', [
           Validators.required,
-          Validators.minLength(3)
+          Validators.minLength(4)
         ]),
-        apellido: new FormControl('',[
-          Validators.maxLength(10)
+        apellidos: new FormControl('', [
+          Validators.maxLength(7)
         ]
         ),
-        edad: new FormControl(''),
-        dni: new FormControl(''),
+        edad: new FormControl('', [
+          this.edadValidador
+        ]),
+        dni: new FormControl('',
+        [
+          this.dniValidador
+        ]),
         password: new FormControl(''),
         repite_password: new FormControl(''),
         email: new FormControl('', [
@@ -45,8 +53,75 @@ export class ModelComponent implements OnInit {
    }
 
   ngOnInit(): void {
+
+    // const emailControl = this.formulario.controls.email;
+    const emailControl = this.formulario.controls['email']; // otra opcion
+    /**
+     * Cuando detecta que se cambia el campo email, salta la funcion
+     */
+    // emailControl.valueChanges.subscribe(valorCampo => {
+    //   console.log(valorCampo);
+    //   //aqui podemos implementar cualquier funcionalidad que depneda de los cambios
+    // });
+
+    /**
+     *
+     * @function pipe
+     *
+     * @method debounceTime
+     * Con esto salta la funcion cada vez que el usuario haya acabado de
+     * introducir un valor, ya sea, cambiando de foco o apretando enter.
+     *
+     */
+    emailControl.valueChanges.pipe(debounceTime(500)).subscribe(valorCampo => {
+      console.log(valorCampo);
+      //aqui podemos implementar cualquier funcionalidad que depneda de los cambios
+    });
   }
 
+  edadValidador(formControl){
+    const value = formControl.value;
+    console.log(value);
+
+    const max = 65;
+    const min = 18;
+
+    if (value >= min && value <= max){
+      return null;
+    }else{
+      return {edadValidador: { max, min}};
+    }
+
+  }
+
+  dniValidador(formControl){
+    const valDNI = formControl.value;
+    const letrasValidas = 'TRWAGMYFPDXBNJZSQVHLCKET';
+
+    if (/^\d{8}[a-zA-Z]$/.test(valDNI)){
+      const numero = valDNI.substr(0, valDNI.length - 1);
+      const letra = valDNI.charAt(valDNI.length - 1); ;
+      console.log(numero);
+      console.log(letra);
+
+      const resto = numero % 23;
+      const letraSeleccionada = letrasValidas.charAt(resto);
+      if(letra.toUpperCase() === letraSeleccionada){
+        // coincide la letra
+        //el dni es correcto
+        console.log('es valido');
+        return null;
+      }else{
+
+        return{ dniValidator: 'La letra no coincide con el número'};
+      }
+
+    }else{
+      return {dniValidator : 'el dni no tiene el formato correcto.'};
+    }
+
+
+  }
   onSubmit(){
     console.log(this.formulario.value);
   }
