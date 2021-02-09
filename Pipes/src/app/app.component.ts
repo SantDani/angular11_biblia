322eed36cbf20280cb490dfe85e6642153b3262b
SantiUAB <profSantiRP@gmail.com>
2021-02-09 12:49:00 +0100

    Revert "S10.1 Custom Pipes"
    This reverts commit 6065c2d50eda3c4ca756bfca95f97483a86119a4.

--- Pipes/src/app/app.component.ts
+++ Pipes/src/app/app.component.ts
@@ -7,10 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pipes';
-
-  texto: string;
-
-  constructor(){
-    this.texto = 'EN un lugar de la mancha';
-  }
 }
