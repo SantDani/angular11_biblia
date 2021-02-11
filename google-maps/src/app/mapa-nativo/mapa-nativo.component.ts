047e1c6d2453e2293a66116b8def833e555afd87
SantiagoUAB <profsantirp@gmail.com>
2021-02-11 16:50:05 +0100

    S12.1 Google Maps Marcadores Eventos en mapa y marcadores Autocompletar busquedas

--- google-maps/src/app/mapa-nativo/mapa-nativo.component.ts
+++ google-maps/src/app/mapa-nativo/mapa-nativo.component.ts
@@ -15,8 +15,10 @@ export class MapaNativoComponent implements OnInit {
   divMap!: ElementRef;
 
   mapa!: google.maps.Map;
-  constructor() {
 
+  markers: google.maps.Marker [];
+  constructor() {
+    this.markers = [];
    }
 
   ngOnInit(): void {
@@ -32,6 +34,7 @@ export class MapaNativoComponent implements OnInit {
 
         // esta funcion necesita el codigo HTML cargado para utilizar .naviveElement
         this.cargarMapa(position);
+        this.cargarAutoComplete();
       });
 
     }else{
@@ -48,5 +51,101 @@ export class MapaNativoComponent implements OnInit {
     };
 
     this.mapa = new google.maps.Map(this.divMap.nativeElement, opciones);
+
+    // Icono, da personalizacion
+
+    const dogeIcon = {
+      url: 'https://assets.coingecko.com/coins/images/5/small/dogecoin.png?1547792256',
+      scaledSize: new google.maps.Size(50, 50)
+    };
+
+    // crear marcadores
+    const markerPosition = new google.maps.Marker({
+      position: this.mapa.getCenter(),
+      // animation: google.maps.Animation.BOUNCE // lugar donde vamos a posicionar el marcador
+      animation: google.maps.Animation.DROP, // lugar donde vamos a posicionar el marcador
+      icon: dogeIcon
+    });
+
+    markerPosition.setMap(this.mapa);
+
+    // eventos sobre el mapa
+    google.maps.event.addListener(this.mapa, 'click', (event: google.maps.MouseEvent) => {
+      const marker = new google.maps.Marker({
+        position: event.latLng,
+        animation: google.maps.Animation.DROP
+      });
+
+
+      marker.setDraggable(true);
+      marker.setMap(this.mapa);
+
+      this.markers.push(marker); // creamos un array de marcadores
+      // eventos en marcadores
+
+      google.maps.event.addListener(marker, 'click', (event: google.maps.MouseEvent) => {
+        marker.setMap(null); // null elimina el marcador al pulsar sobre el marcador
+      });
+
+      google.maps.event.addListener(marker, 'mouseover', (event: google.maps.MouseEvent) => {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+      });
+
+      google.maps.event.addListener(marker, 'mouseout', (event: google.maps.MouseEvent) => {
+        marker.setAnimation(null); // elimino la animacion al marcador
+      });
+
+      // eliminar todos los marcadores
+    });
+  }
+
+  borrarMarcadores(){
+    for (const marker of this.markers) {
+      marker.setMap(null);
+    }
+  }
+
+  cargarAutoComplete(){
+    const autoComplete = new google.maps.places.Autocomplete(
+      document.querySelector('#inputPlaces')
+    );
+
+    google.maps.event.addListener(autoComplete, 'place_changed', event => {
+      const place = autoComplete.getPlace();
+      console.log(place);
+      /* this.mapa.setCenter(place.geometry.location);
+
+      const marker = new google.maps.Marker({
+        position: place.geometry.location
+      });
+
+      marker.setMap(this.mapa); */
+
+      this.calculaRuta(this.mapa.getCenter(), place.geometry.location,
+        google.maps.TravelMode.WALKING);
+
+    });
+  }
+
+  calculaRuta(
+    pOrigen: string | google.maps.LatLng,
+    pDestino: string | google.maps.LatLng,
+    pModoViaje: google.maps.TravelMode = google.maps.TravelMode.DRIVING
+  ){
+    /* */
+
+    const dirSerice = new google.maps.DirectionsService();
+    const dirRendered = new google.maps.DirectionsRenderer();
+
+    dirRendered.setMap(this.mapa);
+
+    dirSerice.route({
+      origin: pOrigen,
+      destination: pDestino,
+      travelMode: pModoViaje
+    }, result => {
+      console.log(result);
+      dirRendered.setDirections(result);
+    });
   }
 }
